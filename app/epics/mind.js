72b94d43dbbec50970c5a59336861140aee6f3e8
Anthony Chung <anthonychung14@gmail.com>
2017-04-12 06:53:02 -0700

    more obs code

--- app/epics/mind.js
+++ app/epics/mind.js
@@ -16,3 +16,24 @@ export const addItemEpic = action$ =>
         })
         .map(result => ({ type: actionTypes.item.SUCCESS, payload: result }));
 
+// import { Observable } from 'rxjs/Observable';
+
+// import actionTypes from '../constants/actionTypes';
+// import { fetchData } from '../constants/api';
+// import { dataRef } from '../store/firebase';
+
+// const apiSource = () => (action$, store) =>
+//     Observable.flatMap(action => Observable.fromPromise(fetchData(options)));
+
+// export const addItemEpic = action$ =>
+//     action$
+//         .ofType(actionTypes.item.ADD)
+//         .do(x => console.log(x, 'addItemEpic'))
+//         .mergeMap((action) =>
+//             Observable.flatMap(x =>
+//                 Observable.fromPromise(fetchData(action.payload))
+//             )
+//             .do(x => console.log('more do stuff', x))
+//             .map(result => ({ type: actionTypes.item.SUCCESS, payload: result }))
+//         })
+
